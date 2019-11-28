670d997dafef374e1cb2205ed151f75d18812577
Jusan Magno <jusanmagno@gmail.com>
2019-11-28 22:29:59 +0000

    começo do backend

--- redDrink/server.js
+++ redDrink/server.js
@@ -11,4 +11,81 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/game.html')
 })
 
+setInterval(() => {
+    io.emit('concurrent_connections', io.engine.clientsCount)
+}, 5000);
 
+io.on('connection', function(socket) {
+    const admin = socket.handshake.query.admin
+    
+    if(io.engine.clientsCount > maxConcurrentConnections && !admin){
+        socket.emit('show-max-concurrent-connections-message')
+        socket.conn.close()
+        return
+    }else{
+        socket.emit('hide-max-concurret-connections-message')
+    }
+
+    const playerState = game.addPlayer(socket.id)
+    socket.emit('bootstrap', game)
+
+    socket.broadcast.emit('player-update', {
+        socketId: socket.id,
+        newState: playerState
+    })
+
+    if(drink){
+        io.emit('drink', {
+            drinkId: drink.drinkId,
+            score: game.players[socket.id].score
+        })
+    } 
+})
+
+socket.on('disconnect', () =>{
+    game.removePlayer(socket.id)
+    socket.broadcast.emit('player-remove', socket.id)
+})
+
+server.listen(3000, function(){
+  console.log('> Server listening on port:',3000)
+});
+
+
+function createGame() {
+    console.log('> Starting new game')
+    let orderPleyer
+    
+    const game ={
+        players: {},
+        order: {},
+        addPlayer,
+        removePlayer,
+        goPlayer,
+        checkDrink,
+        clearScore
+    }
+
+    function addPlayer(socket) {
+        return game.players[socket['socketId']] = {
+            name: socket['name'],
+            score: 0
+        }
+    }
+
+    function removePlayer(socketId) {
+        delete game.players[socketId]
+    }
+
+    function goPlayer(socketId) {
+        
+    }
+
+    function goAction(socketId) {
+        
+    }
+
+    function clearScore(params) {
+        
+    }
+}
